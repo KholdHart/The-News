ae617c02a73002383d117229e0a0d405a5d7217c
Khold Hart <greatpixelyt@gmail.com>
2025-08-09 14:27:14 +0200

    Fix card rendering logic and update image and date handling in news cards

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 import { getNews } from "./api";
 
+const contentWrapper = document.getElementById('contentWrapper');
+
 getNews().then(data => renderNews(data.articles))
 
 function renderNews(newsData) {
     newsData.forEach(news => {
     const data = {
-        urlImage: newsData.urlToImage,
-        data: newsData.publishedAt,
-        title: newsData.title,
+        urlImage: news.urlToImage,
+        data: news.publishedAt,
+        title: news.title,
         description: news.description,
         url: news.url
     }
        const card = `
        <div class="card">
                 <div class="card-image-wrapper">
-                    <img src="https://picsum.photos/200" alt="">
+                    <img src="${data.urlImage}" >
                 </div>
                 <div class="card-content">
-                    <span class="card-date">9 August 2025</span>
+                    <span class="card-date">${data.date}</span>
                     <h2 class="card-title">
-                        <a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, molestiae officiis possimus iure eveniet deserunt inventore quod nesciunt, quis quae veniam at commodi placeat sed nam. Cum placeat cupiditate quisquam.</a>
+                        <a href="${data.url}">${data.title}</a>
                     </h2>
-                    <p class="card-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dignissimos, enim officia exercitationem voluptas officiis ducimus praesentium facilis tempore culpa animi dolorum maxime inventore odit, deleniti ad voluptatum similique fugiat?</p>
+                    <p class="card-description">
+                    ${data.description}
+                    </p>
                 </div>
             </div>
            
        `
 
        //apend the card to the content-wrapper
+       contentWrapper.insertAdjacentHTML('beforeend', card);
 })
 
 }
